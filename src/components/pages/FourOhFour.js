5b22a0cbf2ebc0d493198c444f19a45342f2a5f2
Kathleen Daily <kkdaily31@gmail.com>
2019-06-18 16:15:21 -0400

    import react

--- src/components/pages/FourOhFour.js
+++ src/components/pages/FourOhFour.js
@@ -1,10 +1,12 @@
-const FourOhFour = () =>
+import React from 'react'
+
+function FourOhFour()
 {
     return (
-        <div class="container" id="otherwise">
-            <div class="vertical-center">
-                <h1 class="center-align">404</h1>
-                <h5 class="center-align">That page does not exist</h5>
+        <div className="container" id="otherwise">
+            <div className="vertical-center">
+                <h1 className="center-align">404</h1>
+                <h5 className="center-align">That page does not exist</h5>
             </div>
         </div>
     )
