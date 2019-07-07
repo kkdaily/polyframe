2c592a09bac013e4a8e5f4dc6a58ee360a3f9b1c
Kathleen Daily <kkdaily31@gmail.com>
2019-07-07 16:51:57 -0400

    reroute all nonexistent routes to home page

--- App.jsx
+++ App.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
-import { HashRouter as Router, Route } from 'react-router-dom';
-import Home from './components/pages/Home/Home';
-import Editor from './components/pages/Editor/Editor';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/pages/Home';
+import Editor from './components/pages/Editor';
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Route path="/" exact component={Home} />
-        <Route path="/editor/" exact component={Editor} />
+      <div className="App" style={{
+        display: 'flex',
+        flexFlow: 'wrap',
+        alignItems: 'center',
+        padding: '10px',
+        height: '100vh'
+      }}>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/editor" component={Editor} />
+          <Route component={Home} />
+        </Switch>
       </div>
     </Router>
   );
