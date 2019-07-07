497fb70b11cecdeb9403543fab96b168eaf78791
Kathleen Daily <kkdaily31@gmail.com>
2019-07-07 16:53:14 -0400

    move EditorControls and EditorImageViewer components to views folder

---
+++ components/views/EditorControls/index.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import {
+  Container, Segment, Button, Checkbox
+} from 'semantic-ui-react';
+import Slider from '../Slider/index';
+import './index.less';
+
+class EditorControls extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    this.props.getChildRefs(this.refs);
+  }
+
+  render() {
+    return (
+      <Container className="EditorControls">
+        <Segment.Group>
+          <Segment padded>
+            <input ref="imgFile" type="file" id="userImg" onChange={this.props.onImageUpload} />
+            <Button onClick={this.props.addImage}>Add Image</Button>
+          </Segment>
+          <Segment padded>
+            <Checkbox checked={this.props.showLines} onChange={this.props.handleCheckboxChange} label="Show Lines" defaultChecked />
+          </Segment>
+          <Segment padded>
+            <Slider polygonSize={this.props.polygonSize} handleChange={this.props.handleSliderChange}></Slider>
+          </Segment>
+          <Segment padded>
+            <Button onClick={this.props.polygonIt} disabled={!this.props.imageSrc} primary>
+              Polygon It
+            </Button>
+          </Segment>
+        </Segment.Group>
+      </Container>
+    );
+  }
+}
+
+export default EditorControls;
